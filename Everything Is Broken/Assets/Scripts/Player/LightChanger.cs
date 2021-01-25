15a07de83fea8a6ef4d3a19ee510c12c97c4d46e
Sproeikoei <66425999+Sproeikoei@users.noreply.github.com>
2021-01-25 19:46:02 +0100

    Update LightChanger.cs

--- Everything Is Broken/Assets/Scripts/Player/LightChanger.cs
+++ Everything Is Broken/Assets/Scripts/Player/LightChanger.cs
@@ -2,16 +2,15 @@ using UnityEngine;
 
 public class LightChanger : MonoBehaviour
 {
-    public Light light;
+    public Light light; 
 
     Color defaultColor;
-    // Start is called before the first frame update
+    
     void Start()
     {
         defaultColor = light.color;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(!ToolAnimation.attacking)
